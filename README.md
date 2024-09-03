9e5c36cf3ec5526738cc7eb027025c71ad881611
Ravindu995 <ravindutharaka2013@gmail.com>
2024-09-03 22:58:34 +0530

    Flutter Project Implemented

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# product-app
+# product_app
 Simple Flutter Mobile App with Firebase Authentication using GetX State management. 
---
+++ android/app/src/main/kotlin/com/proDigit/product_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.proDigit.product_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
