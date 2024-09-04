63b995bd27cb6f29be89a6bb520c73b09b38b563
Ravindu995 <ravindutharaka2013@gmail.com>
2024-09-04 12:52:40 +0530

    Firebase configured to the App
    added dependencies and changed build.gradle files

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:product_app/app/bindings/initial_bindings.dart';
 import 'package:product_app/app/routes/routes.dart';
 import 'app/themes/app_theme.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: 'AIzaSyAqGDZGDYBevhIh2EOkH3c65w1QCTllDXU',
+      appId: '1:35441432988:android:86094d363b5ebe84f49059',
+      messagingSenderId: '35441432988',
+      projectId: 'product-app-abb91',
+      storageBucket: 'product-app-abb91.appspot.com',
+    ),
+  );
   runApp(const MyApp());
 }
 
