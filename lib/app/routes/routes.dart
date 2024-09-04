ad5bce0a319d25f44a2aba628ecb7128252aae9d
Ravindu995 <ravindutharaka2013@gmail.com>
2024-09-04 21:18:23 +0530

    WIP

--- lib/app/routes/routes.dart
+++ lib/app/routes/routes.dart
@@ -2,15 +2,22 @@ import 'package:get/get.dart';
 import 'package:product_app/app/bindings/initial_bindings.dart';
 import 'package:product_app/app/routes/routes_name.dart';
 import 'package:product_app/views/home/home_view.dart';
+import 'package:product_app/views/login/login_view.dart';
+import 'package:product_app/views/profile/profile_view.dart';
 import 'package:product_app/views/splash_view.dart';
 
 class AppPages {
   //static const INITIAL = Routes.HOME;
 
   static final routes = [
+    // GetPage(
+    //   name: Routes.splashScreen,
+    //   page: () => const SplashView(),
+    //   binding: InitialBindings(),
+    // ),
     GetPage(
-      name: Routes.splashScreen,
-      page: () => const SplashView(),
+      name: Routes.loginView,
+      page: () => LoginView(),
       binding: InitialBindings(),
     ),
     GetPage(
@@ -23,15 +30,10 @@ class AppPages {
     //   page: () => DetailView(),
     //   binding: InitialBindings(),
     // ),
-    // GetPage(
-    //   name: Routes.LOGIN,
-    //   page: () => LoginView(),
-    //   binding: InitialBindings(),
-    // ),
-    // GetPage(
-    //   name: Routes.SIGN_UP,
-    //   page: () => SignUpView(),
-    //   binding: InitialBindings(),
-    // ),
+    GetPage(
+      name: Routes.profileView,
+      page: () => ProfileView(),
+      binding: InitialBindings(),
+    ),
   ];
 }
--- lib/app/routes/routes_name.dart
+++ lib/app/routes/routes_name.dart
@@ -1,6 +1,8 @@
 class Routes {
-  static const String splashScreen = '/';
+  //static const String splashScreen = '/';
 
-  static const String loginView = '/login_view';
+  static const String loginView = '/';
+  static const String signinView = '/signin_view';
   static const String homeView = '/home_view';
+  static const String profileView = '/profile_view';
 }
